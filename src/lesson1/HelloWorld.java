e1c4ae5f56b86b8abf0619f40cce13027c91f01c
annshu <ganna.shulga@gmail.com>
2018-03-13 19:25:48 +0200

    first commit

---
+++ src/lesson1/HelloWorld.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("test message");
+        boolean bool = true;
+        char c = 'f';
+        int i = 2;
+        long l = 50000000L;
+        double d = 3.14;
+        float f = 3.14f;
+        System.out.println(bool);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
